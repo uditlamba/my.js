3e7c339b3a4d224f47e72aa053d5d075151e2e07
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-04 13:32:10 +0000

    Date

---
+++ 01_basics/DateorTime.js
@@ -0,0 +1,28 @@
+//Dates 
+
+
+
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+
+
+let MyCreatedDate=new Date(2023,0,23)
+
+console.log(MyCreatedDate.toDateString());
+console.log(MyCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+console.log(MyCreatedDate.getTime());
+
+
+
+
+
