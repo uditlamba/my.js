862b41cf3202969a64d4d0f95e8264cc5dc43484
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-04 13:11:54 +0000

    Math and Number

---
+++ 01_basics/Num.js
@@ -0,0 +1,37 @@
+const score=400
+
+const balance= new Number(100)
+
+console.log(score);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(4));
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'));
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+//+++++++++++++Mtahs++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.01));
+console.log(Math.floor(4.96587));
+console.log(Math.sqrt(19.35489));
+console.log(Math.min(4,2,5,6,3));
+console.log(Math.max(9,6,5,2,14,0.2));
+console.log((Math.random()*10)+1);
+// console.log(Math.);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
