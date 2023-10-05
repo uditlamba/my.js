cc0a4a01684b8c8061909618ef2e91c3851bed5f
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-05 12:19:24 +0000

    Strating Array

---
+++ 01_basics/Array.js
@@ -0,0 +1,55 @@
+// Array
+
+// const myArr=[0,2,3,6,5,4,8]
+// const Name=['udit','lamba']
+
+// console.log(myArr);
+
+// const Arr=new Array(4,5,6,2,8)
+// // console.log(Arr[0]);
+
+// //Array Method
+// Arr.push(71)
+// Arr.push(52)
+// console.log(Arr);
+
+// Arr.pop()
+// console.log(Arr);
+
+// Arr.unshift(110)   // for adding elemenyt in first index
+// console.log(Arr);
+
+// Arr.shift()      // for removing first index element
+// console.log(Arr);
+
+// console.log(Arr.includes(9));
+// console.log(Arr.indexOf(4));
+
+// const newArr=Arr.join()
+// console.log(Arr);
+// console.log(typeof Arr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice ,splice
+
+// console.log("A",Arr);
+// const myN=Arr.slice(1,3)
+// console.log(Arr);
+
+const A=[1,2,3,4,35,0,22]
+const B=[5,10,19,7,8] 
+// console.log(A.concat(B));
+
+// console.log(A.lastIndexOf(3));
+A.sort();
+console.log(A);
+B.sort();
+console.log(B);
+
+
+
+
+
+
+
