1bf52f061f67e695ff39c82a6c6a139d122d3ec8
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-06 12:25:59 +0000

    function

---
+++ 01_basics/function.js
@@ -0,0 +1,29 @@
+// console.log("U");
+
+function sayMyName(){
+    console.log("U");
+    console.log("D");
+    console.log("I");
+    console.log("T");
+}
+sayMyName()
+
+function addTwoNum(num1,num2){     //num1& num2 are parameter
+    console.log(num1+num2);
+
+}
+function addTwoNum(num1,num2){     //num1& num2 are parameter
+    // let result=num1+num2
+    // return result
+    return num1+num2
+
+}
+const result=addTwoNum(4,"a")   //4 & a are argument
+// console.log("Result",result);
+
+function loginUser(username) {
+    return '${username} just logged in'
+    
+}
+console.log(loginUser("udit"));
+
