ab3f24ea11af32e9da4198eac96315a33e19f95d
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-06 13:05:38 +0000

    function with object

---
+++ 01_basics/functionWithObj.js
@@ -0,0 +1,30 @@
+function calculateCartPrice(...num1){
+    return num1
+
+}
+// console.log(calculateCartPrice(200,400,500));
+
+const user={
+    username:"Udit",
+    price:199
+}
+function handleObject(anyobject){
+    console.log('Username is ${anyobject.username} and price is ${anyobject.price}');
+
+}
+handleObject({
+    username:"udit",
+    price:300   
+
+})
+
+const myNewArry=[200,500,360]
+function Value(getArray){
+    return getArray[2]
+
+}
+console.log(Value(myNewArry));
+console.log(Value([140,15,420]));
+
+
+ 
