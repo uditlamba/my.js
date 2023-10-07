43e73c11dc3585ac9a0e65e45dab2761fae85e2a
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-07 06:14:31 +0000

    scope in function

---
+++ 01_basics/Scopelevel.js
@@ -0,0 +1,45 @@
+function one(){
+    const username="udit"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+        console.log(website);
+
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const userName="Udit"
+    if(userName==="udit"){
+        const Website=" youtube"
+        // console.log(userName + Website);
+        console.log(Website);
+    }
+    // console.log(Website);
+    // console.log(userName);
+}
+// console.log(userName);
+
+
+
+
+//++++++++++++++++interesting++++++++++++++++++++
+
+
+function addone(num){
+    // console.log(num);
+    return num +1
+    // console.log(num);
+}
+console.log(addone(5));
+
+// console.log(addTwo(9))
+const addTwo=function addTwo(num1){
+    return num1+2
+}
+console.log(addTwo(8));
+
