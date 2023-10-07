ca0974369a47258468505d9828c540c912dc2fd2
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-07 16:47:39 +0000

    Switch caseWith default value

---
+++ 01_basics/Switch.js
@@ -0,0 +1,43 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("Mai");
+        break;
+    case 6:
+        console.log("JUN");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("Sept");
+        break;
+
+    default:
+        console.log("Use default case");
+        break;
+}
