fa923006311555e01df641a06de4a76d25eaa24a
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-08 12:45:00 +0000

    for in loop

---
+++ 01_basics/forIn.js
@@ -0,0 +1,19 @@
+const myObj={
+    js:'javascipt',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift for apple'
+}
+
+
+
+for (const key in myObj) {
+    
+        // const element = object[key];
+        console.log(key,":",myObj[key]);
+    
+}
+const programmig=["js","rb","c++","java","python"]
+for (const key in programmig) {
+    console.log(programmig[key]);
+}
