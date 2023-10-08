bf623037ee284395860fd317bcfc218598a35485
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-08 07:36:39 +0000

    do while loop

---
+++ 01_basics/doWhile.js
@@ -0,0 +1,20 @@
+//do while loop
+
+// let score=1
+
+// do {
+//     console.log(score);
+//     score++
+    
+// } while (score<10);
+
+
+//one loop run complusery
+  
+let score=11
+
+do {
+    console.log(score);
+    score++
+    
+} while (score<10);
