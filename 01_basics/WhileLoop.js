97e9b4ecc6067d692ca178a7778e88ca7af722b4
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-08 07:31:07 +0000

    While Loop

---
+++ 01_basics/WhileLoop.js
@@ -0,0 +1,12 @@
+// let i=0
+// while(i<5){
+//     console.log(i);
+//     i=i+2
+// }
+
+let myArr=['flash','batman','superman']
+let arr=0
+while(arr<myArr.length){
+    console.log(myArr[arr]);
+    arr++
+}
