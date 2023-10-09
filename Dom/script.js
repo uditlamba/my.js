d73f20159b9f69b7a2ae0185fb1f50aafa1bbe69
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-09 21:15:56 +0530

    seaching Dom

---
+++ Dom/script.js
@@ -0,0 +1,20 @@
+// let ctitle=document.getElementsByClassName('card-title')[0]
+// ctitle.style.h5="redn"
+
+// let ctitles=document.quarySelectorAll()
+// let id1=document.getElementById('id1')
+// console.log(id1)
+// console.log(id1.matches(".box"))
+// console.log(id2.matches(".box"))
+// console.log(sp1.closest("#id1"))
+// console.log(sp1.closest(".box1"))
+// console.log(id1.contains(sp1))
+// console.log(sp1.contains(id1))
+
+
+let A=document.getElementsByTagName("li")[1]
+A.style.color="red"
+let B=document.querySelectorAll("#we")[0]
+B.style.color="green"
+
+
