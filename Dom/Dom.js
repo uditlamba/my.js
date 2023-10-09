e3d1d8582df15f71dda59d1dee6160405e5efcce
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-09 21:14:33 +0530

    Add files via upload

---
+++ Dom/Dom.js
@@ -0,0 +1,35 @@
+const box1=document.getElementById('box-1');
+// box1.innerHTML="<h1>Udit</h1></br><p>lamba</p> ";
+
+// box1.style.backgroundColor = "red";
+// box1.classList.add('round-border');
+
+
+
+function myfun(){
+    console.log("i click here");
+}
+
+// const boxes=document.getElementsByClassName("box");
+// for(let i=0;i<boxes.length;i++){
+//     boxes[i].classList.add('round-border');
+// }
+ 
+
+// const myBody=document.body;
+
+// console.log(myBody);
+
+// const box2=document.getElementById('box-2');
+// console.log(box2);
+// const para=document.getElementsByTagName("p");
+// console.log(para);
+// const boxes=document.getElementsByClassName('box');
+
+// console.log(boxes);
+// const random=document.querySelectorAll('.container .random');
+// console.log(random);
+// const divs=document.getElementsByTagName('div');
+// console.log(divs).style.color = "green";
+// const D=document.title("DOM Manipulation");
+// console.log(D) 
